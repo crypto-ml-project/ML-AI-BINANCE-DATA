13ad2f793eda2c0b25ba5e8dd8dc6a473e70e384
justTil <t.schwarze@student.xu-university.de>
2023-02-03 16:31:45 +0100

    #1 Created file for checking timestamp gap

---
+++ Utilities/Date Utilities/CheckTimestampGap.py
@@ -0,0 +1,36 @@
+import datetime
+import csv
+import os
+from datetime import datetime, timezone
+
+
+def unix_ms_to_iso_gmt(unix_timestamp_ms):
+    unix_timestamp = unix_timestamp_ms / 1000
+    iso_time = datetime.fromtimestamp(
+        unix_timestamp, timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+    return iso_time
+
+
+def check_timestamps(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        timestamps = [row[0] for row in reader]
+        for i in range(1, len(timestamps)):
+
+            ts1 = unix_ms_to_iso_gmt(int(timestamps[i-1]))
+            ts2 = unix_ms_to_iso_gmt(int(timestamps[i]))
+
+            ts3 = timestamps[i-1]
+            ts4 = timestamps[i]
+
+            if float(ts4) - float(ts3) != float(300000):
+                print(
+                    f"Timestamps {unix_ms_to_iso_gmt(int(ts3))} and {unix_ms_to_iso_gmt(int(ts4))} are not 5 minutes apart. {float(ts4) - float(ts3)} Row: {i}")
+            else:
+                test = 0
+
+
+folder = "./data/BTCUSDT/Test"
+for file in os.listdir(folder):
+    if file.endswith(".csv"):
+        check_timestamps(os.path.join(folder, file))
